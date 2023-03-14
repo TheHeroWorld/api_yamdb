92f0db9957f94ee13425b32cf27957499002617d
Alexander Karelin <karelin888@gmail.com>
2023-03-14 18:46:00 +0300

    1st

---
+++ api_yamdb/api_yamdb/reviews/models.py
@@ -0,0 +1,4 @@
+from django.core.validators import (MaxValueValidator, MinValueValidator,
+                                    validate_slug)
+from django.db import models
+from users.models import User
