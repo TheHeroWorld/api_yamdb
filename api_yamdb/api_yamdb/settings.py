f5d6e3f1e6992037e2201c357170d45c6bc1966f
Pavel Lashkov <hutjicsgo@gmail.com>
2023-03-15 22:54:03 +0300

    added classes Review, Comment in models

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 
@@ -39,7 +40,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
